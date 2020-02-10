a3fa06f4de91967f26e14c08083440bd970cf2b3
jFriedli <jonas.friedli@hotmail.com>
2020-02-10 22:41:08 +0100

    init

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(CharacterController))]
+public class PlayerControler : MonoBehaviour
+{
+    public float rotationSpeed = 540f;
+    public float speed = 10;
+
+    private CharacterController controller;
+    private Vector3 clickDestinationPosition;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        doRotation();
+
+        doMovement();
+    }
+
+    void doRotation()
+    {
+        Vector3 rotationDirection = locateMousePosition();
+        transform.LookAt(rotationDirection * rotationSpeed);
+    }
+
+    void doMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+
+    Vector3 locateMousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            return new Vector3(hit.point.x, hit.point.y, hit.point.z);
+        }
+
+        return new Vector3(0f, 0f , 0f);
+    }
+}
